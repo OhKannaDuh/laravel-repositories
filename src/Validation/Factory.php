6bc8c7ea7872eb3e44d8bebbfa30b44cf82dbde3
Liam McNair <Liam@mediablanket.co.uk>
2021-11-30 10:29:33 +0000

    SEt presence verifier on validator create

--- src/Validation/Factory.php
+++ src/Validation/Factory.php
@@ -10,7 +10,10 @@ class Factory extends ValidationFactory
     protected function resolve(array $data, array $rules, array $messages, array $customAttributes): Validator
     {
         if (is_null($this->resolver)) {
-            return new Validator($this->translator, $data, $rules, $messages, $customAttributes);
+            $validator = new Validator($this->translator, $data, $rules, $messages, $customAttributes);
+            $validator->setPresenceVerifier(app('validation.presence'));
+
+            return $validator;
         }
 
         return call_user_func($this->resolver, $this->translator, $data, $rules, $messages, $customAttributes);
